04a7527faf9443da28ebfafd8aae30e70f213b31
Kyle Plump <kplump@encentivenergy.com>
2024-08-21 17:21:48 -0400

    start of stack implementation

--- main.go
+++ main.go
@@ -2,34 +2,65 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"image"
-	_ "image/jpeg"
-	// "bufio"
+	// "os"
+	// "image"
+	// _ "image/jpeg"
 )
 
+
+type Color struct {
+	r uint32
+	g uint32
+	b uint32
+	a uint32
+	run uint32
+}
+
+type Stack struct {
+	items []Color
+}
+
+type Stackable interface {
+	Push() []Color
+	Pop() Color
+}
+
+func (s Stack) Push(v Color) Stack {
+	s.items = append(s.items, v);
+	return s;
+}
+
 func main() {
-	fmt.Println("hello world");
-	imageFile, err := os.Open("./test.jpeg");
+	fmt.Println("start");
+	var stack Stack
 
-	if err != nil {
-		// handle error
-		fmt.Println("error opening image");
-	}
+	fmt.Println("stack: ", stack)
+	color := Color{r: 123, g: 123, b:3254345, a: 1}
+	stack = stack.Push(color)
+	fmt.Println("stack 2: ", stack)
 
-	imageData, _, err := image.Decode(imageFile)
+	// imageFile, err := os.Open("./test.jpeg");
 
-	fmt.Println("image Data: ", imageData.Bounds())
-	bounds := imageData.Bounds();
+	// if err != nil {
+	// 	// handle error
+	// 	fmt.Println("error opening image");
+	// }
 
-	for i := bounds.Min.X; i < bounds.Max.X; i ++ {
-		for j := bounds.Min.Y; j < bounds.Max.Y; j ++ {
-			r, g, b, a := imageData.At(i, j).RGBA()
-			fmt.Println("r: ", r)
-			fmt.Println("g: ", g)
-			fmt.Println("b: ", b)
-			fmt.Println("a: ", a)
-			// fmt.Println("point: ", imageData.At(i, j).RGBA())
-		}
-	}
+	// imageData, _, err := image.Decode(imageFile)
+
+	// fmt.Println("image Data: ", imageData.Bounds())
+	// bounds := imageData.Bounds();
+	// var data []Color
+
+	// for i := bounds.Min.X; i < bounds.Max.X; i ++ {
+	// 	for j := bounds.Min.Y; j < bounds.Max.Y; j ++ {
+	// 		r, g, b, a := imageData.At(i, j).RGBA()
+
+	// 		fmt.Println("r: ", r)
+	// 		fmt.Println("g: ", g)
+	// 		fmt.Println("b: ", b)
+	// 		fmt.Println("a: ", a)
+	// 		// fmt.Println("point: ", imageData.At(i, j).RGBA())
+	// 	}
+	// }
 }
